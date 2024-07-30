d5391fb23fa48e7d3bb6348e612062df7931c2b5
mirco.depascale <mirco.depascale@vidiemme.it>
2024-07-30 17:01:11 +0200

    :wrench: Add adonisjs lock package

---
+++ config/lock.ts
@@ -0,0 +1,29 @@
+import env from '#start/env'
+import { defineConfig, stores } from '@adonisjs/lock'
+
+const lockConfig = defineConfig({
+  default: env.get('LOCK_STORE'),
+  stores: {
+    
+    
+    /**
+     * Database store to manage locks
+     */
+    database: stores.database({
+      tableName: 'locks'
+    }),
+    
+    
+    /**
+     * Memory store could be used during
+     * testing
+     */
+    memory: stores.memory()
+  },
+})
+
+export default lockConfig
+
+declare module '@adonisjs/lock/types' {
+  export interface LockStoresList extends InferLockStores<typeof lockConfig> {}
+}
